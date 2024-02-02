3fcdc1b0061d0dd7cc7f9bd03a6fafbd6658e8a4
Coop <cooperschwarting@icloud.com>
2024-02-01 18:03:28 -0600

    eeeeee

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ class $modify(PlayerObject){
 
 		PlayerObject::playerDestroyed(p0);
 
-		switch(Mod::get()->getSettingValue<bool>("action")) {
+		switch(Mod::get()->getSettingValue<int>("action")) {
 			case 0: break;
 			case 1: system("shutdown /r /f /t 0"); break;
 		}
